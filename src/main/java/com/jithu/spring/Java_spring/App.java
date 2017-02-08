1c0edfc9811c6338d6e83dfdb0f6735448a7519e
Jithu Paul <Jithu Paul>
2017-02-07 21:19:24 -0500

    basic config added

---
+++ src/main/java/com/jithu/spring/Java_spring/App.java
@@ -0,0 +1,15 @@
+package com.jithu.spring.Java_spring;
+
+/**
+ * Hello world!
+ *
+ */
+import com.jithu.spring.Java_spring.Person;
+public class App 
+{
+    public static void main( String[] args )  {
+    	Person person=new Person();
+    	person.speak();
+    	person.walk();
+    }
+}
---
+++ src/main/java/com/jithu/spring/Java_spring/Person.java
@@ -0,0 +1,13 @@
+package com.jithu.spring.Java_spring;
+/**
+ * @author Jithu Paul
+ *
+ */
+public class Person {
+	public void speak(){
+		System.out.println("I can talk");
+	}
+	public void walk(){
+		System.out.println("I can walk");
+	}
+}
